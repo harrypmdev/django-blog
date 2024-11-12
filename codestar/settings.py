5cf676d9860c8de50a09389c3ea2754c2cac5f83
Harry Miles <harrypetermiles@gmail.com>
2024-11-12 21:31:11 +0000

    fix: update ALLOWED_HOSTS

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-harrypmdev-djangoblog-9flo70syblz.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-harrypmdev-djangoblog-hkoar0vyv10.ws-eu116.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
