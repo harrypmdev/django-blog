9a4f628fa4b8b21f22e4d292bacc581ee9ea7230
Harry Miles <harrypetermiles@gmail.com>
2024-11-16 15:48:21 +0000

    feat: add PostList view and template

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-harrypmdev-djangoblog-050qxavpju7.ws.codeinstitute-ide.net', 
 'https://8000-harrypmdev-djangoblog-9flo70syblz.ws.codeinstitute-ide.net/', 
